91dd91070be1a3a2e7dbcef4f44cd97f6bde88a7
Momchil <momchilgenov2001@gmail.com>
2024-12-12 21:03:11 +0200

    Initial project setup for single server client in spring mvc

---
+++ ExpenseTrackerMVCClient/src/test/java/com/example/ExpenseTrackerMVCClient/ExpenseTrackerMvcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExpenseTrackerMVCClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerMvcClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
