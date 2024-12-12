fe0a33c57a3bc206970af530dc9c795a1bce354c
Momchil <momchilgenov2001@gmail.com>
2024-12-12 22:29:39 +0200

    Initial Setup Service Layer Project

---
+++ service-core/src/test/java/com/momchilgenov/springboot/service_core/ServiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momchilgenov.springboot.service_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
