7d611680a4f8d82dbc0822a8b8db500c5c645a03
Momchil <momchilgenov2001@gmail.com>
2024-12-12 22:41:39 +0200

    Renaming project for conventions

---
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/MvcwebApplication.java
@@ -0,0 +1,13 @@
+package com.momchilgenov.springboot.mvcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcwebApplication.class, args);
+	}
+
+}
