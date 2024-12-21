c58e382b91dd48100171160b6ec4d2aa0eccd245
Momchil <momchilgenov2001@gmail.com>
2024-12-21 04:53:42 +0200

    Added basic login controller and form with homepage

---
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/login/LoginController.java
@@ -0,0 +1,20 @@
+package com.momchilgenov.springboot.mvcweb.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        return "homepage";
+    }
+
+}
