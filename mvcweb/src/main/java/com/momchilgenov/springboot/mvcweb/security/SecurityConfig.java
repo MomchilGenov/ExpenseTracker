a3520f7208360392fb3917da8e0815f6e42c4b23
Momchil <momchilgenov2001@gmail.com>
2025-01-11 02:44:46 +0200

    now adds custom filter to chain

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/security/SecurityConfig.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.momchilgenov.springboot.mvcweb.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,10 +9,19 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    private final JwtAuthFilter filter;
+
+    @Autowired
+    public SecurityConfig(JwtAuthFilter authFilter) {
+        this.filter = authFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,6 +39,9 @@ public class SecurityConfig {
                         .logoutSuccessUrl("/login")
                         .permitAll()
                 );
+
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
