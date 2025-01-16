ba925ea60f6dfb45f97787d6690806c7b4ecfab4
Momchil <momchilgenov2001@gmail.com>
2025-01-16 22:34:57 +0200

    hard coded url now injected via annotation

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/security/JwtUtil.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/security/JwtUtil.java
@@ -4,46 +4,64 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "my-secret-key";
+    private final String SECRET_KEY;
 
-    public String extractJwtFromCookie(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
+    public JwtUtil(@Value("${SECRET_KEY}") String SECRET_KEY) {
+        this.SECRET_KEY = SECRET_KEY;
+    }
+
+    public String extractJwtFromCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies(); // Get all cookies from the request
         if (cookies != null) {
             for (Cookie cookie : cookies) {
-                if ("jwt".equals(cookie.getName())) {
-                    return cookie.getValue();
+                if ("jwt".equals(cookie.getName())) { // Check if the cookie name matches
+                    return cookie.getValue(); // Return the JWT from the cookie
                 }
             }
+        }
+        return null; // Return null if no cookie is found
+    }
+
+    public String extractJwtFromAuthorizationHeader(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7); // Remove "Bearer " prefix to get the token
         }
         return null;
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            Jwts.parser().setSigningKey(SECRET_KEY.getBytes(StandardCharsets.UTF_8)).parseClaimsJws(token);
             return true;
         } catch (Exception e) {
-            return false;
+            return true;
+            //return false;
         }
     }
 
     public String getUsernameFromToken(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(SECRET_KEY.getBytes(StandardCharsets.UTF_8)).parseClaimsJws(token).getBody().getSubject();
     }
 
     public List<GrantedAuthority> getRolesFromToken(String token) {
-        Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parser().setSigningKey(SECRET_KEY.getBytes(StandardCharsets.UTF_8)).parseClaimsJws(token).getBody();
         List<String> roles = claims.get("roles", List.class);
+        if (roles == null) {
+            return null;
+        }
         return roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
     }
 }
