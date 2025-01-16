23f77b6c41687398ca634fe55c53e8f130f1e8cb
Momchil <momchilgenov2001@gmail.com>
2025-01-16 20:42:10 +0200

    Proper communication with backend, will impl real details

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/login/BackendService.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/login/BackendService.java
@@ -1,19 +1,24 @@
 package com.momchilgenov.springboot.mvcweb.login;
 
-import org.springframework.context.annotation.Bean;
+import com.momchilgenov.springboot.mvcweb.entity.User;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class BackendService {
+    private final String URL_OF_JWT_AUTHENTICATOR = "custom url";
 
-    private final String URL_OF_JWT_AUTHENTICATOR = "enter valid ip address here";
+    public String authenticateUser(String username, String password) {
+        String str = "This is a trivial public message";
 
-    @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl("URL_OF_JWT_AUTHENTICATOR") // Set a default base URL if needed
-                .build();
+        System.out.println("in backendservice");
+        User user = new User(username, password);
+
+        // Send the login credentials to the backend
+        RestTemplate restTemplate = new RestTemplate();
+        String jwtToken = restTemplate.postForObject(URL_OF_JWT_AUTHENTICATOR, user, String.class);
+
+        return jwtToken;
     }
-
 
 }
