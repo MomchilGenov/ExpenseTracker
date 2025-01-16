b9929dfa63d3d0a9bea194c7658c655259d4d40b
Momchil <momchilgenov2001@gmail.com>
2025-01-16 22:37:00 +0200

    refactoring, added auth header check for jwt too

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/security/JwtAuthFilter.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/security/JwtAuthFilter.java
@@ -2,6 +2,7 @@ package com.momchilgenov.springboot.mvcweb.security;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,19 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        String jwt = jwtUtil.extractJwtFromCookie(request);
+        System.out.println("in JwtAuthFilter");
+        String jwt = jwtUtil.extractJwtFromAuthorizationHeader(request);
+        if (jwt == null) {
+            System.out.println("No jwt in authorization header");
+            jwt = jwtUtil.extractJwtFromCookies(request);
+            if (jwt == null) {
+                System.out.println("Neither in cookies");
+            }
+        }
 
-        if (jwt != null && jwtUtil.validateToken(jwt)) {
+
+        if (jwt != null) {//&& jwtUtil.validateToken(jwt)) {
+            System.out.println("received a jwt in filter");
             String username = jwtUtil.getUsernameFromToken(jwt);
 
             // Create an authentication token to access in custom authentication provider
@@ -40,7 +51,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-
+        //System.out.println("Fake authenticated");
+        //SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("IvanTest", null, null));
         filterChain.doFilter(request, response);
     }
 }
