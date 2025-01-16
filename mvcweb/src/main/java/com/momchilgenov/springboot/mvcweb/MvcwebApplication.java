1064d6dc0d36efc0b09c1f3710700917d78e4a77
Momchil <momchilgenov2001@gmail.com>
2025-01-16 20:30:23 +0200

    Refactored code to separate classes

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/MvcwebApplication.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/MvcwebApplication.java
@@ -1,13 +1,33 @@
 package com.momchilgenov.springboot.mvcweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class MvcwebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MvcwebApplication.class, args);
-	}
+    //todo
+   /*
+    Implement a dummy jwt token, test it passes jwtUtil functions
+    Save the jwt itself as a cookie
+    Check for a cookie on jwtauthfilter
+
+   */
+    public static void main(String[] args) {
+
+        SpringApplication.run(MvcwebApplication.class, args);
+    }
+
+
 
 }
+
+
+
