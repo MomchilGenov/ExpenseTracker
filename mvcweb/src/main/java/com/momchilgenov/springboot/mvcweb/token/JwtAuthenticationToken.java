4d39865f877964e224cb09c890ba7bf70e1e81dc
Momchil <momchilgenov2001@gmail.com>
2025-02-23 14:37:29 +0200

    Refactor, new token package

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/token/JwtAuthenticationToken.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/token/JwtAuthenticationToken.java
@@ -1,4 +1,4 @@
-package com.momchilgenov.springboot.mvcweb.auth;
+package com.momchilgenov.springboot.mvcweb.token;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
