04ebafba3212b4ab33ee6085ea03d10504e86d25
Momchil <momchilgenov2001@gmail.com>
2025-03-03 23:11:05 +0200

    whitespace cleanup

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/ServicecoreApplication.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/ServicecoreApplication.java
@@ -1,7 +1,9 @@
 package com.momchilgenov.springboot.servicecore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServicecoreApplication {
@@ -9,5 +11,4 @@ public class ServicecoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServicecoreApplication.class, args);
 	}
-
 }
