04c81c033cebb558405fca5580567a30754805df
Momchil <momchilgenov2001@gmail.com>
2025-03-04 18:04:38 +0200

    dummy impl added,decoupled from service

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthRepository.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthRepository.java
@@ -1,8 +1,21 @@
 package com.momchilgenov.springboot.servicecore.auth;
 
+import com.momchilgenov.springboot.servicecore.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class AuthRepository {
+
+    public User authenticateUser(User user) {
+        //can replace with a dummy in memory db for more users and role testing.
+        List<String> dummyRoles = new ArrayList<>();
+        dummyRoles.add("ROLE_USER");
+        dummyRoles.add("ROLE_ADMIN");
+        return new User("Ivan Samuilov", null, dummyRoles);
+    }
+
 }
