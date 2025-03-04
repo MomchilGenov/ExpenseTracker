3f5e6aaa8b13fb1f7d2987c1a681582602958461
Momchil <momchilgenov2001@gmail.com>
2025-03-04 17:57:19 +0200

    Added List<String> roles field with getter setter and in cstr

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/User.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/User.java
@@ -1,17 +1,22 @@
 package com.momchilgenov.springboot.servicecore;
 
+import java.util.List;
+
 public class User {
 
     private String username;
     private String password;
 
+    private List<String> roles;
+
     public User() {
 
     }
 
-    public User(String username, String password) {
+    public User(String username, String password, List<String> roles) {
         this.username = username;
         this.password = password;
+        this.roles = roles;
     }
 
     public String getUsername() {
@@ -28,6 +33,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
     }
 
     @Override
