b04768efc605d666e37855ea4206083bae46844f
Momchil <momchilgenov2001@gmail.com>
2025-03-04 17:31:22 +0200

    Method to call repo and perform validation and jwt generation with dummy impl for now

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthService.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthService.java
@@ -1,4 +1,17 @@
 package com.momchilgenov.springboot.servicecore.auth;
 
+import com.momchilgenov.springboot.servicecore.User;
+import com.momchilgenov.springboot.servicecore.token.JwtAccessToken;
+import com.momchilgenov.springboot.servicecore.token.JwtRefreshToken;
+import com.momchilgenov.springboot.servicecore.token.JwtTokenPair;
+
 public class AuthService {
+
+    public JwtTokenPair authenticateUser(User user) {
+        //todo - implement
+        JwtAccessToken accessToken = new JwtAccessToken("Dummy access token"); //todo - implement generation with helper class
+        JwtRefreshToken refreshToken = new JwtRefreshToken("Dummy refresh token"); //todo - implement generation with helper class
+        JwtTokenPair jwtTokenPair = new JwtTokenPair(accessToken, refreshToken);
+        return jwtTokenPair;
+    }
 }
