9c596b5381fd3a61b25c8c4c54ffc2ff0b266b1c
Momchil <momchilgenov2001@gmail.com>
2025-03-04 17:42:51 +0200

    Added Repository annotation

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthRepository.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthRepository.java
@@ -1,4 +1,8 @@
 package com.momchilgenov.springboot.servicecore.auth;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+@Repository
 public class AuthRepository {
 }
