8bb11f40b2d4a3763ab5f37a9fe9d34d23409c43
Momchil <momchilgenov2001@gmail.com>
2025-03-09 17:46:49 +0200

    Uses jwtUtil to generate tokens and added jwtUtil as a field

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthService.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthService.java
@@ -1,17 +1,40 @@
 package com.momchilgenov.springboot.servicecore.auth;
 
 import com.momchilgenov.springboot.servicecore.User;
+import com.momchilgenov.springboot.servicecore.security.JwtUtil;
 import com.momchilgenov.springboot.servicecore.token.JwtAccessToken;
 import com.momchilgenov.springboot.servicecore.token.JwtRefreshToken;
 import com.momchilgenov.springboot.servicecore.token.JwtTokenPair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class AuthService {
 
+    private final AuthRepository authRepository;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public AuthService(AuthRepository authRepository, JwtUtil jwtUtil) {
+        this.authRepository = authRepository;
+        this.jwtUtil = jwtUtil;
+    }
+
     public JwtTokenPair authenticateUser(User user) {
-        //todo - implement
-        JwtAccessToken accessToken = new JwtAccessToken("Dummy access token"); //todo - implement generation with helper class
-        JwtRefreshToken refreshToken = new JwtRefreshToken("Dummy refresh token"); //todo - implement generation with helper class
-        JwtTokenPair jwtTokenPair = new JwtTokenPair(accessToken, refreshToken);
-        return jwtTokenPair;
+
+        //validates user exists in db and password matches
+        User userDto = authRepository.authenticateUser(user);
+        if (userDto == null) {
+            //todo - throw userNotFoundException, catch in controller, send 404
+            return null;
+        }
+
+        //todo - (for token repo revocation logic) - save the token/jti in the repo upon creation/logout to keep track of revoked
+        //and blacklisted tokens
+        //todo keep track of last issued token timestamp to blacklist refresh tokens
+        JwtAccessToken accessToken = jwtUtil.generateJwtAccessToken(userDto);
+        JwtRefreshToken refreshToken = jwtUtil.generateJwtRefreshToken(userDto);
+        return new JwtTokenPair(accessToken, refreshToken);
     }
 }
