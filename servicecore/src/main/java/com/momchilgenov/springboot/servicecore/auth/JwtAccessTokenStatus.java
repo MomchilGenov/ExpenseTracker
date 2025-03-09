1f344205cb21270368b08e427825257232718d16
Momchil <momchilgenov2001@gmail.com>
2025-03-09 18:29:43 +0200

    Added doc to response entity class

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/JwtAccessTokenStatus.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/JwtAccessTokenStatus.java
@@ -1,5 +1,14 @@
 package com.momchilgenov.springboot.servicecore.auth;
 
+/**
+ * @param iss - issuer, either JwtClaimValidationStatus.VALID or JwtClaimValidationStatus.INVALID
+ * @param aud - issuer, either JwtClaimValidationStatus.VALID or JwtClaimValidationStatus.INVALID
+ * @param exp - true if expired, false otherwise
+ * @param sub - JwtClaimValidationStatus.EXISTS if everything is ok, JwtClaimValidationStatus.BANNED if user is banned,
+ *            JwtClaimValidationStatus.NOT_FOUND if no such user exists
+ * @param sub_roles_match - true if user exists and roles in token match given user's roles (true for BANNED users too),
+ *                        so always check sub claim status too.
+ */
 public record JwtAccessTokenStatus(JwtClaimValidationStatus iss, JwtClaimValidationStatus aud,
                                    boolean exp, JwtClaimValidationStatus sub, boolean sub_roles_match) {
 }
