c3b6595b22b05fdbfaedc2b3816dda083ec3e1c6
Momchil <momchilgenov2001@gmail.com>
2025-03-23 19:46:36 +0200

    stub for validating roles match user

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthRepository.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthRepository.java
@@ -7,15 +7,26 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Repository
 public class AuthRepository {
 
+    //todo rename auth repo to AuthenticationDatabaseApiClient or smth like that
     public User authenticateUser(User user) {
         //can replace with a dummy in memory db for more users and role testing.
+        //todo - check for user in a db, if not present, return null, this is directly linked with
+        //JwtAuthProvider return statement and placing the same username that was received.
+
         List<String> dummyRoles = new ArrayList<>();
         dummyRoles.add("ROLE_USER");
         dummyRoles.add("ROLE_ADMIN");
         return new User("Ivan Samuilov", null, dummyRoles);
     }
+
+    //todo - implement real impl, rename class to a service/client
+    public JwtAccessTokenStatus validateSubjectExistsAndRolesMatch(String username, List<String> roles) {
+        return new JwtAccessTokenStatus(null, null, false, JwtClaimValidationStatus.EXISTS, true);
+    }
+
 
 }
