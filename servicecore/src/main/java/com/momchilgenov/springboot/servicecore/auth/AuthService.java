21672ad175c86d6f03cbba60b5d7a3ac610518f4
Momchil <momchilgenov2001@gmail.com>
2025-03-27 00:39:10 +0200

    Revoking tokens prior to issuing in authenticateUser()

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthService.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthService.java
@@ -33,10 +33,7 @@ public class AuthService {
             //todo - throw userNotFoundException, catch in controller, send 404
             return null;
         }
-
-        //todo - (for token repo revocation logic) - save the token/jti in the repo upon creation/logout to keep track of revoked
-        //and blacklisted tokens
-        //todo keep track of last issued token timestamp to blacklist refresh tokens
+        tokenService.revokeAll(userDto.getUsername());
         JwtAccessToken accessToken = jwtUtil.generateJwtAccessToken(userDto);
         JwtRefreshToken refreshToken = jwtUtil.generateJwtRefreshToken(userDto);
         return new JwtTokenPair(accessToken, refreshToken);
