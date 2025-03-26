ce6a314d6be0a98f2a3313fd58c78dcc37795dc1
Momchil <momchilgenov2001@gmail.com>
2025-03-27 00:02:19 +0200

    isRevoked method added + javadocs for the 2 methods

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/TokenService.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/TokenService.java
@@ -21,9 +21,23 @@ public class TokenService {
         this.revokedTokens = new ConcurrentHashMap<>();
     }
 
-    //revokes all tokens for a given user issued before the present moment
+    /**
+     * revokes all tokens for a given user issued before the present moment
+     *
+     * @param username - the user for whom all tokens issued up to current moment should be revoked
+     */
     public void revokeAll(String username) {
         revokedTokens.put(username, Date.from(Instant.now()));
+    }
+
+    /**
+     * @param username - the username of the user
+     * @param iatClaim - the token claim to verify against to see if the token from
+     *                 which the claim was extracted is revoked
+     * @return - true, if the token is revoked, false otherwise
+     */
+    public boolean isRevoked(String username, Date iatClaim) {
+        return this.revokedTokens.get(username).after(iatClaim);
     }
 
 }
