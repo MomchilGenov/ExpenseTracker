e0d333549bbcf61684b3d0bb0749ac9b610ac69d
Momchil <momchilgenov2001@gmail.com>
2025-04-02 05:41:59 +0300

    Comment cleanup

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/MvcwebApplication.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/MvcwebApplication.java
@@ -1,5 +1,9 @@
 package com.momchilgenov.springboot.mvcweb;
 
+import com.momchilgenov.springboot.mvcweb.entity.User;
+import com.momchilgenov.springboot.mvcweb.security.JwtUtil;
+import com.momchilgenov.springboot.mvcweb.token.dto.JwtAccessToken;
+import com.momchilgenov.springboot.mvcweb.token.dto.JwtAccessTokenStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,26 +11,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class MvcwebApplication {
 
-    //todo
-   /*
-    Implement a dummy jwt token, test it passes jwtUtil functions
-    Save the jwt itself as a cookie
-    Check for a cookie on jwtauthfilter
 
-   */
     public static void main(String[] args) {
 
         SpringApplication.run(MvcwebApplication.class, args);
     }
-
-
-
 }
 
 
