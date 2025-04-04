a9691424dbf25e3ec039f3dd85fc5f405fe7a8e1
Momchil <momchilgenov2001@gmail.com>
2025-04-05 00:12:16 +0300

    usernameCollides is a boolean

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/auth/UserRegistrationStatus.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/auth/UserRegistrationStatus.java
@@ -3,5 +3,5 @@ package com.momchilgenov.springboot.mvcweb.auth;
 
 import com.momchilgenov.springboot.mvcweb.token.dto.JwtTokenPair;
 
-public record UserRegistrationStatus(String usernameCollides, JwtTokenPair tokenPair) {
+public record UserRegistrationStatus(boolean usernameCollides, JwtTokenPair tokenPair) {
 }
