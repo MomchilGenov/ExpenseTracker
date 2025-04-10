baab0c3db0c0f4f863102f9d88d08f6ab396dafa
Momchil <momchilgenov2001@gmail.com>
2025-04-10 12:43:59 +0300

    A controller for expense related features

---
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/expense/ExpenseController.java
@@ -0,0 +1,9 @@
+package com.momchilgenov.springboot.mvcweb.expense;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/expense")
+public class ExpenseController {
+}
