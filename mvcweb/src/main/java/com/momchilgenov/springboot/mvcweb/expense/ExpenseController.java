e8c03a5e831c249e5b501c4522db11529ca0de81
Momchil <momchilgenov2001@gmail.com>
2025-04-10 12:54:44 +0300

    Fixed api endpoint naming

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/expense/ExpenseController.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/expense/ExpenseController.java
@@ -1,9 +1,14 @@
 package com.momchilgenov.springboot.mvcweb.expense;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/expense")
+@RequestMapping("/api/v1/expenses")
 public class ExpenseController {
+
+   
+
+
 }
