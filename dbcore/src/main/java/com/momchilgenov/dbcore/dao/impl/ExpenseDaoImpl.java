36dca4dbb7ed3a4a91c04b0b30c1cc90d5d5dc6e
Momchil <momchilgenov2001@gmail.com>
2025-04-15 23:36:12 +0300

    Added transactional and repository annotations

--- dbcore/src/main/java/com/momchilgenov/dbcore/dao/impl/ExpenseDaoImpl.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/dao/impl/ExpenseDaoImpl.java
@@ -5,9 +5,12 @@ import com.momchilgenov.dbcore.entity.Expense;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
 public class ExpenseDaoImpl implements ExpenseDao {
 
     private final EntityManager entityManager;
@@ -18,22 +21,26 @@ public class ExpenseDaoImpl implements ExpenseDao {
     }
 
     @Override
+    @Transactional
     public Expense findById(Long id) {
         return this.entityManager.find(Expense.class, id);
     }
 
     @Override
+    @Transactional
     public List<Expense> findAll() {
         TypedQuery<Expense> query = entityManager.createQuery("FROM Expense", Expense.class);
         return query.getResultList();
     }
 
     @Override
+    @Transactional
     public void save(Expense expense) {
         this.entityManager.persist(expense);
     }
 
     @Override
+    @Transactional
     public Expense update(Expense expense) {
         return this.entityManager.merge(expense);
     }
