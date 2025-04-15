7a224f4d9e02a86e8b23dd61803bc8c1d05da24d
Momchil <momchilgenov2001@gmail.com>
2025-04-15 20:24:23 +0300

    Category entity

---
+++ dbcore/src/main/java/com/momchilgenov/dbcore/entity/Category.java
@@ -0,0 +1,43 @@
+package com.momchilgenov.dbcore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    //if null creates new, else tries to update, if 0 -> mostly undefined behaviour
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+    public Category() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
