7ebb617daba94858217662c6f8d9a0a187d76e93
Momchil <momchilgenov2001@gmail.com>
2025-04-16 00:41:11 +0300

    Basic userRole connection impl

---
+++ dbcore/src/main/java/com/momchilgenov/dbcore/entity/UserRole.java
@@ -0,0 +1,44 @@
+package com.momchilgenov.dbcore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_roles")
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "role_id")
+    private Long roleId;
+
+    public UserRole() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUser_id() {
+        return userId;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.userId = user_id;
+    }
+
+    public Long getRole_id() {
+        return roleId;
+    }
+
+    public void setRole_id(Long role_id) {
+        this.roleId = role_id;
+    }
+}
