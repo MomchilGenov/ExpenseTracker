f0c802a2c08f2fbce9f54655678d690b71455015
Momchil <momchilgenov2001@gmail.com>
2025-04-16 17:51:00 +0300

    Expense entity impl with db mappings

--- dbcore/src/main/java/com/momchilgenov/dbcore/entity/Expense.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/entity/Expense.java
@@ -1,4 +1,37 @@
 package com.momchilgenov.dbcore.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "expenses")
 public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "amount")
+    private double amount;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "date")
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User expenseCreator;
+
 }
