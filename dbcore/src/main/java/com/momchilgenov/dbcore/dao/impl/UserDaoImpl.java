0bb60c07217408cdb313840ab58e700769686406
Momchil <momchilgenov2001@gmail.com>
2025-04-16 05:21:29 +0300

    Implemented all methods and manually tested

--- dbcore/src/main/java/com/momchilgenov/dbcore/dao/impl/UserDaoImpl.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/dao/impl/UserDaoImpl.java
@@ -2,6 +2,8 @@ package com.momchilgenov.dbcore.dao.impl;
 
 import com.momchilgenov.dbcore.dao.UserDao;
 import com.momchilgenov.dbcore.dao.dto.UserAuthenticationStatus;
+import com.momchilgenov.dbcore.dao.dto.UserCredentialsStatus;
+import com.momchilgenov.dbcore.entity.Role;
 import com.momchilgenov.dbcore.entity.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
@@ -9,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Repository
@@ -33,10 +38,11 @@ public class UserDaoImpl implements UserDao {
         TypedQuery<User> query = this.entityManager.
                 createQuery("FROM User u WHERE u.username=:username", User.class);
         query.setParameter("username", username);
-        User foundUser = query.getSingleResult();
-        if (foundUser == null) {
+        List<User> singleUser = query.getResultList();
+        if (singleUser == null || singleUser.isEmpty()) {
             return null;
         }
+        User foundUser = singleUser.get(0);
         User result = new User();
         result.setUsername(foundUser.getUsername());
         result.setRoles(foundUser.getRoles());
@@ -47,12 +53,52 @@ public class UserDaoImpl implements UserDao {
     @Transactional
     @Override
     public User authenticateUser(User user) {
-        return null;
+        TypedQuery<User> query = this.entityManager.
+                createQuery("FROM User u WHERE u.username=:username", User.class);
+        query.setParameter("username", user.getUsername());
+        List<User> singleUser = query.getResultList();
+        if (singleUser == null || singleUser.isEmpty()) {
+            return null;
+        }
+        User foundUser = singleUser.get(0);
+        //return null if no such user is found,passwords do not match or is banned
+        if (foundUser == null || !foundUser.getPassword().equals(user.getPassword()) || !foundUser.isEnabled()) {
+            return null;
+        }
+        User result = new User();
+        result.setEnabled(true);
+        result.setRoles(foundUser.getRoles());
+        result.setUsername(foundUser.getUsername());
+        //possibly redundant and security issue
+        result.setId(foundUser.getId());
+        return result;
     }
 
     @Transactional
     @Override
     public UserAuthenticationStatus validateSubjectExistsAndRolesMatch(String username, List<String> roles) {
-        return null;
+        TypedQuery<User> query = this.entityManager.
+                createQuery("FROM User u WHERE u.username=:username", User.class);
+        query.setParameter("username", username);
+        List<User> singleUser = query.getResultList();
+        if (singleUser == null || singleUser.isEmpty()) {
+            return null;
+        }
+        User foundUser = singleUser.get(0);
+        //return null if no such user is found,passwords do not match or is banned
+        if (foundUser == null || !foundUser.isEnabled()) {
+            return new UserAuthenticationStatus(UserCredentialsStatus.NOT_FOUND, null, null);
+        }
+        UserAuthenticationStatus result =
+                new UserAuthenticationStatus(UserCredentialsStatus.EXISTS,
+                        null, UserCredentialsStatus.ROLES_MISMATCH);
+        Set<String> textRoles = foundUser.getRoles().stream().map(Role::getName).collect(Collectors.toSet());
+        if (textRoles.equals(new HashSet<>(roles))) {
+            result =
+                    new UserAuthenticationStatus(UserCredentialsStatus.EXISTS,
+                            null, UserCredentialsStatus.ROLES_MATCH);
+        }
+
+        return result;
     }
 }
