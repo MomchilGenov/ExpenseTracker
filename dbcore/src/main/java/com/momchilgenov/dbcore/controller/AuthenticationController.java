228301dae0c35e4d73dc8e607ad6345f64e9db36
Momchil <momchilgenov2001@gmail.com>
2025-04-22 18:31:37 +0300

    added authentication service field to controller

--- dbcore/src/main/java/com/momchilgenov/dbcore/controller/AuthenticationController.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/controller/AuthenticationController.java
@@ -1,11 +1,29 @@
 package com.momchilgenov.dbcore.controller;
 
+import com.momchilgenov.dbcore.dao.dto.UserDto;
+import com.momchilgenov.dbcore.entity.Role;
+import com.momchilgenov.dbcore.entity.User;
+import com.momchilgenov.dbcore.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
 
 
 }
