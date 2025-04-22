a7492bdc45b8bfed77d0b32e05b757b2a1fbba3f
Momchil <momchilgenov2001@gmail.com>
2025-04-22 18:20:02 +0300

    userDao added to ctor

--- dbcore/src/main/java/com/momchilgenov/dbcore/service/AuthenticationService.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/service/AuthenticationService.java
@@ -1,7 +1,19 @@
 package com.momchilgenov.dbcore.service;
 
+import com.momchilgenov.dbcore.dao.UserDao;
+import com.momchilgenov.dbcore.dao.dto.UserDto;
+import com.momchilgenov.dbcore.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+    private final UserDao userDao;
+
+    @Autowired
+    public AuthenticationService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
 }
