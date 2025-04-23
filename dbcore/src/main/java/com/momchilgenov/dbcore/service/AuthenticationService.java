3d2e9cb14f18ceb46f56e0b8763a6dc674a179c1
Momchil <momchilgenov2001@gmail.com>
2025-04-23 18:45:08 +0300

    register method implemented

--- dbcore/src/main/java/com/momchilgenov/dbcore/service/AuthenticationService.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/service/AuthenticationService.java
@@ -1,18 +1,28 @@
 package com.momchilgenov.dbcore.service;
 
+import com.momchilgenov.dbcore.dao.RoleDao;
 import com.momchilgenov.dbcore.dao.UserDao;
 import com.momchilgenov.dbcore.dao.dto.UserDto;
+import com.momchilgenov.dbcore.entity.Role;
 import com.momchilgenov.dbcore.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class AuthenticationService {
     private final UserDao userDao;
+    private final RoleDao roleDao;
+    private final String DEFAULT_ROLE = "ROLE_USER";
 
     @Autowired
-    public AuthenticationService(UserDao userDao) {
+    public AuthenticationService(UserDao userDao, RoleDao roleDao) {
         this.userDao = userDao;
+        this.roleDao = roleDao;
     }
 
     public UserDto findUserByUsername(String username) {
@@ -33,5 +43,23 @@ public class AuthenticationService {
         }
 
         return new UserDto(foundUser);
+    }
+
+    public boolean register(UserDto user) {
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(user.getPassword());
+        newUser.setEnabled(true);
+        Set<Role> roles = new HashSet<>();
+        Optional<Role> userRole = roleDao.findByName(DEFAULT_ROLE);
+        if (userRole.isPresent()) {
+            Role defaultRole = userRole.get();
+            roles.add(defaultRole);
+        } else {
+            throw new RuntimeException("Default role does not exist in database");
+        }
+        newUser.setRoles(roles);
+        userDao.save(newUser);
+        return true;
     }
 }
