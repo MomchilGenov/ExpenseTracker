d2bf7d9c75060138995a9a1e90efc305c9840dd8
Momchil <momchilgenov2001@gmail.com>
2025-04-23 18:43:39 +0300

    findByName method added

--- dbcore/src/main/java/com/momchilgenov/dbcore/dao/RoleDao.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/dao/RoleDao.java
@@ -3,5 +3,8 @@ package com.momchilgenov.dbcore.dao;
 import com.momchilgenov.dbcore.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleDao extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
