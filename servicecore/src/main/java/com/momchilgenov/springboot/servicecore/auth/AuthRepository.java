fcafca864270bd9b48929e0fe15ac0ba3094a33a
Momchil <momchilgenov2001@gmail.com>
2025-04-29 02:07:20 +0300

    validateSubjectExistsAndRolesMatch impl + url final fields

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthRepository.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/auth/AuthRepository.java
@@ -13,13 +13,16 @@ public class AuthRepository {
     private final String URL_OF_FIND_USER_BY_USERNAME;
     private final String URL_OF_AUTHENTICATE_USER;
     private final String URL_OF_REGISTER_USER;
+    private final String URL_OF_VALIDATE_AUTHORITY;
 
     public AuthRepository(@Value("${URL_OF_FIND_USER_BY_USERNAME}") String URL_OF_FIND_USER_BY_USERNAME,
                           @Value("${URL_OF_AUTHENTICATE_USER}") String URL_OF_AUTHENTICATE_USER,
-                          @Value("${URL_OF_REGISTER_USER}") String URL_OF_REGISTER_USER) {
+                          @Value("${URL_OF_REGISTER_USER}") String URL_OF_REGISTER_USER,
+                          @Value("${URL_OF_VALIDATE_AUTHORITY}") String URL_OF_VALIDATE_AUTHORITY) {
         this.URL_OF_FIND_USER_BY_USERNAME = URL_OF_FIND_USER_BY_USERNAME;
         this.URL_OF_AUTHENTICATE_USER = URL_OF_AUTHENTICATE_USER;
         this.URL_OF_REGISTER_USER = URL_OF_REGISTER_USER;
+        this.URL_OF_VALIDATE_AUTHORITY = URL_OF_VALIDATE_AUTHORITY;
     }
 
     //todo rename auth repo to AuthenticationDatabaseApiClient or smth like that
@@ -27,9 +30,23 @@ public class AuthRepository {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject(URL_OF_AUTHENTICATE_USER, user, User.class);
     }
-
-    //todo - implement real impl, rename class to a service/client
+    
     public JwtAccessTokenStatus validateSubjectExistsAndRolesMatch(String username, List<String> roles) {
+        RestTemplate restTemplate = new RestTemplate();
+        User user = new User();
+        user.setUsername(username);
+        user.setRoles(roles);
+        AuthorityValidationDto result = restTemplate.postForObject(URL_OF_VALIDATE_AUTHORITY, user,
+                AuthorityValidationDto.class);
+        if (result.username() == null) {
+            return new JwtAccessTokenStatus(null, null, false,
+                    JwtClaimValidationStatus.NOT_FOUND, false);
+        }
+        if (result.roles() == null) {
+            return new JwtAccessTokenStatus(null, null, false,
+                    JwtClaimValidationStatus.EXISTS, false);
+
+        }
         return new JwtAccessTokenStatus(null, null, false, JwtClaimValidationStatus.EXISTS, true);
     }
 
