cd3c58c01b73248377a06ef9f15db8a5eaf8112a
Momchil <momchilgenov2001@gmail.com>
2025-04-29 02:00:58 +0300

    Dto for validation status of user name and roles

---
+++ dbcore/src/main/java/com/momchilgenov/dbcore/dao/dto/AuthorityValidationDto.java
@@ -0,0 +1,25 @@
+package com.momchilgenov.dbcore.dao.dto;
+
+import java.util.List;
+
+
+public class AuthorityValidationDto {
+    private String username;
+    private List<String> roles;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
