499a5a43b6f6463e7613ba1cc8af3deafeb762a8
Momchil <momchilgenov2001@gmail.com>
2025-04-30 02:19:27 +0300

    changed id from int to Long

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/Category.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/Category.java
@@ -1,18 +1,18 @@
 package com.momchilgenov.springboot.mvcweb.category;
 
 public class Category {
-    private int id;
+    private Long id;
     private String name;
 
     public Category(String name) {
         this.name = name;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
