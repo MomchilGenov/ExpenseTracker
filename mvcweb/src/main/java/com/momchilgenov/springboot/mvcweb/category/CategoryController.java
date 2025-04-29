e4b843812b8b6f4c35fd5ddda7e7797854864c0b
Momchil <momchilgenov2001@gmail.com>
2025-04-30 01:05:55 +0300

    getmapping method to show dashboard with dummy data

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/CategoryController.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/CategoryController.java
@@ -1,10 +1,33 @@
 package com.momchilgenov.springboot.mvcweb.category;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1/categories")
 public class CategoryController {
+    @GetMapping("")
+    public String showCategoriesDashBoard(Model model) {
+        List<Category> categories = new ArrayList<>();
+        Category category1 = new Category("Travel");
+        Category category2 = new Category("Fun");
+        Category category3 = new Category("Tech");
+        Category category4 = new Category("Debts");
+        Category category5 = new Category("Other");
+        categories.add(category1);
+        categories.add(category2);
+        categories.add(category3);
+        categories.add(category4);
+        categories.add(category5);
+        model.addAttribute("categories", categories);
+        return "categories/list";
+    }
+
     
+
 }
