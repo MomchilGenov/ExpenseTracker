addbdcd1c211a1933a4082ccb38a583761bc4f5a
Momchil <momchilgenov2001@gmail.com>
2025-05-01 05:38:24 +0300

    category impl of EntityClient

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/CategoryClient.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/CategoryClient.java
@@ -1,4 +1,35 @@
 package com.momchilgenov.springboot.mvcweb.category;
 
-public class CategoryClient {
+import com.momchilgenov.springboot.mvcweb.client.EntityClient;
+import com.momchilgenov.springboot.mvcweb.dto.EntityWithUserDTO;
+
+import java.util.List;
+
+public class CategoryClient implements EntityClient<Category> {
+
+
+    @Override
+    public List<Category> findAll(String username) {
+        return null;
+    }
+
+    @Override
+    public void create(EntityWithUserDTO<Category> entityDto) {
+
+    }
+
+    @Override
+    public Category getById(String username, Long id) {
+        return null;
+    }
+
+    @Override
+    public void update(EntityWithUserDTO<Category> entityDto) {
+
+    }
+
+    @Override
+    public void delete(String username, Long id) {
+
+    }
 }
