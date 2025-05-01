6250f59e3124fc25fb54ccbbeb7eec5bd9028c6e
Momchil <momchilgenov2001@gmail.com>
2025-05-01 19:15:57 +0300

    CategoryClient field injected

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/CategoryService.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/CategoryService.java
@@ -1,7 +1,15 @@
 package com.momchilgenov.springboot.mvcweb.category;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+    private final CategoryClient categoryClient;
+
+    @Autowired
+    public CategoryService(CategoryClient categoryClient) {
+        this.categoryClient = categoryClient;
+    }
+
 }
