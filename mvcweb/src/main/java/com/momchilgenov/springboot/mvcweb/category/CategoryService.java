ecb26d05344c7e0092465ffdf59d3b6014eb87f2
Momchil <momchilgenov2001@gmail.com>
2025-05-01 19:19:41 +0300

    create implemented

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/CategoryService.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/CategoryService.java
@@ -20,6 +20,10 @@ public class CategoryService {
         return this.categoryClient.findAll(username);
     }
 
+    public void create(EntityWithUserDTO<Category> entityDto) {
+        this.categoryClient.create(entityDto);
+    }
+
    
 
 }
