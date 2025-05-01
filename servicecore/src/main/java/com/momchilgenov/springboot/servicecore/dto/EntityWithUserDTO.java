93c693c6811b8140f618fdd99620c4b00974a48c
Momchil <momchilgenov2001@gmail.com>
2025-05-01 21:31:40 +0300

    dto object for entities and a username

---
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/dto/EntityWithUserDTO.java
@@ -0,0 +1,52 @@
+package com.momchilgenov.springboot.servicecore.dto;
+
+
+public class EntityWithUserDTO<T> {
+    private String username;
+    private T entity;
+
+    private Long id;
+
+    public EntityWithUserDTO() {
+
+    }
+
+    public EntityWithUserDTO(String username, T entity, Long id) {
+        this.username = username;
+        this.entity = entity;
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public T getEntity() {
+        return entity;
+    }
+
+    public void setEntity(T entity) {
+        this.entity = entity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "EntityWithUserDTO{" +
+                "username='" + username + '\'' +
+                ", entity=" + entity +
+                ", id=" + id +
+                '}';
+    }
+}
