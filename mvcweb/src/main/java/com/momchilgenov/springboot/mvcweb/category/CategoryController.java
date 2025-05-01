8036fbeaee4e249d1e1f6118598a4aa5452e6c05
Momchil <momchilgenov2001@gmail.com>
2025-05-01 19:12:27 +0300

    categoryService field injected

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/CategoryController.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/category/CategoryController.java
@@ -1,5 +1,6 @@
 package com.momchilgenov.springboot.mvcweb.category;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,14 @@ import java.util.List;
 @Controller
 @RequestMapping("/api/v1/categories")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("")
     public String showCategoriesDashBoard(Model model) {
         List<Category> categories = new ArrayList<>();
