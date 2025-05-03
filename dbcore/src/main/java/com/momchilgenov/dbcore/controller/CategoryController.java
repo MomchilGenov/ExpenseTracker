06c470b0c630760370d843a7390fe54e3fcc8209
Momchil <momchilgenov2001@gmail.com>
2025-05-03 04:07:26 +0300

    findAll stub with dummy data

--- dbcore/src/main/java/com/momchilgenov/dbcore/controller/CategoryController.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/controller/CategoryController.java
@@ -15,5 +15,34 @@ import java.util.List;
 @RequestMapping("api/categories")
 public class CategoryController {
 
+    @PostMapping("/findAll")
+    public List<Category> findAll(@RequestBody String username) {
+        System.out.println("Received username in findAll method = " + username);
+        List<Category> categories = new ArrayList<>();
+        Category category1 = new Category();
+        category1.setName("Travel");
+        Category category2 = new Category();
+        category2.setName("Fun");
+        Category category3 = new Category();
+        category3.setName("Tech");
+        Category category4 = new Category();
+        category4.setName("Debts");
+        Category category5 = new Category();
+        category5.setName("Other");
+
+        category1.setId(1L);
+        category2.setId(2L);
+        category3.setId(3L);
+        category4.setId(4L);
+        category5.setId(5L);
+        categories.add(category1);
+        categories.add(category2);
+        categories.add(category3);
+        categories.add(category4);
+        categories.add(category5);
+
+
+        return categories;
+    }
 
 }
