2c6c66d2b2f68304fc8d1bf0eace962998705b56
Momchil <momchilgenov2001@gmail.com>
2025-05-03 04:06:12 +0300

    A controller for category requests

---
+++ dbcore/src/main/java/com/momchilgenov/dbcore/controller/CategoryController.java
@@ -0,0 +1,19 @@
+package com.momchilgenov.dbcore.controller;
+
+import com.momchilgenov.dbcore.dto.CategoryDto;
+import com.momchilgenov.dbcore.dto.EntityWithUserDto;
+import com.momchilgenov.dbcore.entity.Category;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/categories")
+public class CategoryController {
+
+
+}
