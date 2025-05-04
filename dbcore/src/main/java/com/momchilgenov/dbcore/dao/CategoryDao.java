def6b2efcca6878001abff0b1a833f3829fdc304
Momchil <momchilgenov2001@gmail.com>
2025-05-04 04:39:29 +0300

    standard 4 methods but for a specific user by userId

--- dbcore/src/main/java/com/momchilgenov/dbcore/dao/CategoryDao.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/dao/CategoryDao.java
@@ -1,15 +1,27 @@
 package com.momchilgenov.dbcore.dao;
 
 import com.momchilgenov.dbcore.entity.Category;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CategoryDao {
     Category findById(Long id);
 
+    Category findById(Long categoryId, Long userId);
+
     List<Category> findAll();
+
+    List<Category> findAllByUserId(Long userId);
 
     void save(Category category);
 
+    void saveForUser(Category category, Long userId);
+
     Category update(Category category);
+
+    Category updateForUser(Category category, Long userId);
+
+
 }
