d5e22df38a00397297f2a7e2888683961fa5e432
Momchil <momchilgenov2001@gmail.com>
2025-05-04 05:23:57 +0300

    stub methods for standard dao operations

--- dbcore/src/main/java/com/momchilgenov/dbcore/service/CategoryService.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/service/CategoryService.java
@@ -1,8 +1,30 @@
 package com.momchilgenov.dbcore.service;
 
+import com.momchilgenov.dbcore.dto.CategoryDto;
+import com.momchilgenov.dbcore.entity.Category;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
+    public List<Category> findAll(String username) {
+        return null;
+    }
 
+    public CategoryDto getById(Long categoryId, String username) {
+        return null;
+    }
+
+    public void save(CategoryDto category, String username) {
+
+    }
+
+    public void update(CategoryDto categoryDto, String username) {
+
+    }
+
+    public void delete(Long categoryId) {
+        
+    }
 }
