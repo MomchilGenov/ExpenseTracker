36a566a3834fdf0b123f73dc8f7ee01ecd30d47c
Momchil <momchilgenov2001@gmail.com>
2025-05-04 05:16:22 +0300

    CategoryService dependency injected

--- dbcore/src/main/java/com/momchilgenov/dbcore/controller/CategoryController.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.momchilgenov.dbcore.controller;
 import com.momchilgenov.dbcore.dto.CategoryDto;
 import com.momchilgenov.dbcore.dto.EntityWithUserDto;
 import com.momchilgenov.dbcore.entity.Category;
+import com.momchilgenov.dbcore.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -11,6 +13,13 @@ import java.util.List;
 @RestController
 @RequestMapping("api/categories")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @PostMapping("/findAll")
     public List<Category> findAll(@RequestBody String username) {
