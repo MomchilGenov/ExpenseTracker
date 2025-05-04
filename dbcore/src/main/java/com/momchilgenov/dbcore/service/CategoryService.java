aa93d4e53a0992f2f15d7d5d1e0269f9bf75ac8c
Momchil <momchilgenov2001@gmail.com>
2025-05-04 05:28:11 +0300

    userDao dependency injected

--- dbcore/src/main/java/com/momchilgenov/dbcore/service/CategoryService.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/service/CategoryService.java
@@ -1,8 +1,10 @@
 package com.momchilgenov.dbcore.service;
 
 import com.momchilgenov.dbcore.dao.CategoryDao;
+import com.momchilgenov.dbcore.dao.UserDao;
 import com.momchilgenov.dbcore.dto.CategoryDto;
 import com.momchilgenov.dbcore.entity.Category;
+import com.momchilgenov.dbcore.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,12 @@ import java.util.List;
 @Service
 public class CategoryService {
     private final CategoryDao categoryDao;
+    private final UserDao userDao;
 
     @Autowired
-    public CategoryService(CategoryDao categoryDao) {
+    public CategoryService(CategoryDao categoryDao, UserDao userDao) {
         this.categoryDao = categoryDao;
+        this.userDao = userDao;
     }
 
     public List<Category> findAll(String username) {
