c0a92cbecad9c275605e0f5f22f0ced0f1f7d960
Momchil <momchilgenov2001@gmail.com>
2025-05-06 01:47:24 +0300

    ExpenseService method stubs added

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/expense/ExpenseService.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/expense/ExpenseService.java
@@ -1,7 +1,31 @@
 package com.momchilgenov.springboot.mvcweb.expense;
 
+import com.momchilgenov.springboot.mvcweb.dto.EntityWithUserDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ExpenseService {
+
+    public List<Expense> findAll(String username) {
+        return null;
+    }
+
+    public void create(EntityWithUserDTO<Expense> entityDto) {
+    }
+
+    public Expense getById(String username, Long id) {
+        return null;
+    }
+
+    public void update(EntityWithUserDTO<Expense> entityDto) {
+
+    }
+
+    public void delete(String username, Long id) {
+
+    }
+
+
 }
