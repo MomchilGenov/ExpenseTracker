dc9b4c0de1e853851fe390f98a4be51f135419a1
Momchil <momchilgenov2001@gmail.com>
2025-05-06 03:38:41 +0300

    api contract developed with respective calls to service

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/expense/ExpenseController.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/expense/ExpenseController.java
@@ -1,8 +1,10 @@
 package com.momchilgenov.springboot.servicecore.expense;
 
+import com.momchilgenov.springboot.servicecore.dto.EntityWithUserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -12,6 +14,32 @@ public class ExpenseController {
     @Autowired
     public ExpenseController(ExpenseService expenseService) {
         this.expenseService = expenseService;
+    }
+
+    @GetMapping("/getById")
+    public ExpenseDto getById(@RequestBody EntityWithUserDTO<ExpenseDto> entityDto) {
+        return expenseService.getById(entityDto.getUsername(), entityDto.getId());
+    }
+
+    @GetMapping("/findAll")
+    public List<ExpenseDto> findAll(@RequestBody String username) {
+        return expenseService.findAll(username);
+    }
+
+    @PostMapping("/create")
+    public void create(@RequestBody EntityWithUserDTO<ExpenseDto> entityDto) {
+        expenseService.create(entityDto);
+    }
+
+
+    @PutMapping("/update")
+    public void update(@RequestBody EntityWithUserDTO<ExpenseDto> entityDto) {
+        expenseService.update(entityDto);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestBody EntityWithUserDTO<ExpenseDto> entityDto) {
+        expenseService.delete(entityDto.getUsername(), entityDto.getId());
     }
 
 }
