d42982f38cf03cc52874d82c7285e5faede620ce
Momchil <momchilgenov2001@gmail.com>
2025-05-06 03:27:32 +0300

    ExpenseController with injected ExpenseService dependency

---
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/expense/ExpenseController.java
@@ -0,0 +1,17 @@
+package com.momchilgenov.springboot.servicecore.expense;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class ExpenseController {
+    private final ExpenseService expenseService;
+
+    @Autowired
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+}
