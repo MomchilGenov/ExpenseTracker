b8356ff1a3b6d5790c72b18df431bc6383b4a95d
Momchil <momchilgenov2001@gmail.com>
2025-05-06 04:12:51 +0300

    template impls of all standard dao methods

--- dbcore/src/main/java/com/momchilgenov/dbcore/controller/ExpenseController.java
+++ dbcore/src/main/java/com/momchilgenov/dbcore/controller/ExpenseController.java
@@ -1,9 +1,19 @@
 package com.momchilgenov.dbcore.controller;
 
+import com.momchilgenov.dbcore.dto.EntityWithUserDto;
+import com.momchilgenov.dbcore.dto.ExpenseDto;
+import com.momchilgenov.dbcore.entity.Expense;
 import com.momchilgenov.dbcore.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/expenses")
@@ -15,4 +25,41 @@ public class ExpenseController {
     public ExpenseController(ExpenseService expenseService) {
         this.expenseService = expenseService;
     }
+
+    @GetMapping("/findAll")
+    public List<Expense> findAll(@RequestBody String username) {
+        return expenseService.findAll(username);
+    }
+
+    @GetMapping("/getById")
+    public ExpenseDto getById(@RequestBody EntityWithUserDto<ExpenseDto> entityDto) {
+        String username = entityDto.getUsername();
+        Long expenseId = entityDto.getId();
+        return expenseService.getById(expenseId, username);
+    }
+
+    @PostMapping("/create")
+    public void createExpense(@RequestBody EntityWithUserDto<ExpenseDto> entityDto) {
+        String username = entityDto.getUsername();
+        ExpenseDto expenseDto = entityDto.getEntity();
+        expenseService.save(expenseDto, username);
+    }
+
+    @PutMapping("/update")
+    public void updateExpense(@RequestBody EntityWithUserDto<ExpenseDto> entityDto) {
+        String username = entityDto.getUsername();
+        Long entityId = entityDto.getId();
+        ExpenseDto expenseDto = entityDto.getEntity();
+        expenseService.update(expenseDto, username);
+    }
+
+
+    @DeleteMapping("/delete")
+    public void deleteExpense(@RequestBody EntityWithUserDto<ExpenseDto> entityDto) {
+        String username = entityDto.getUsername();
+        Long expenseId = entityDto.getId();
+        expenseService.delete(expenseId);
+    }
+
+
 }
