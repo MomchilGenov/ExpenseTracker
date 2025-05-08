46d5e345622cb4558cdcd44554195dd5e1ebe1ac
Momchil <momchilgenov2001@gmail.com>
2025-05-08 08:57:45 +0300

    RestTemplate DI

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/expense/ExpenseClient.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/expense/ExpenseClient.java
@@ -1,16 +1,29 @@
 package com.momchilgenov.springboot.servicecore.expense;
 
+import com.momchilgenov.springboot.servicecore.category.CategoryDto;
 import com.momchilgenov.springboot.servicecore.client.EntityClient;
 import com.momchilgenov.springboot.servicecore.dto.EntityWithUserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 public class ExpenseClient implements EntityClient<ExpenseDto> {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public ExpenseClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
     public List<ExpenseDto> findAll(String username) {
-        return null;
+
     }
 
     @Override
