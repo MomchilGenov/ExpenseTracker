6f987c4f62a09ed5798352498d08ec871b1cb822
Momchil <momchilgenov2001@gmail.com>
2025-05-08 08:37:12 +0300

    @RequestMapping value fixed

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/expense/ExpenseController.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/expense/ExpenseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categories")
+@RequestMapping("/expenses")
 public class ExpenseController {
     private final ExpenseService expenseService;
 
