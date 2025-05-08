d7b22ee7f146f2c7c42ae066098ee23551d9c897
Momchil <momchilgenov2001@gmail.com>
2025-05-08 09:02:26 +0300

    URLs injected via @Value and in constructor and as fields

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/expense/ExpenseClient.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/expense/ExpenseClient.java
@@ -4,6 +4,7 @@ import com.momchilgenov.springboot.servicecore.category.CategoryDto;
 import com.momchilgenov.springboot.servicecore.client.EntityClient;
 import com.momchilgenov.springboot.servicecore.dto.EntityWithUserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,27 @@ import java.util.List;
 @Service
 public class ExpenseClient implements EntityClient<ExpenseDto> {
 
+    private final String URL_OF_FIND_ALL_EXPENSES;
+    private final String URL_OF_CREATE_EXPENSE;
+    private final String URL_OF_GET_EXPENSE_BY_ID;
+    private final String URL_OF_UPDATE_EXPENSE;
+    private final String URL_OF_DELETE_EXPENSE;
+
     private final RestTemplate restTemplate;
 
     @Autowired
-    public ExpenseClient(RestTemplate restTemplate) {
+    public ExpenseClient(RestTemplate restTemplate,
+                         @Value("${URL_OF_FIND_ALL_EXPENSES}") String URL_OF_FIND_ALL_EXPENSES,
+                         @Value("${URL_OF_CREATE_EXPENSE}") String URL_OF_CREATE_EXPENSE,
+                         @Value("${URL_OF_GET_EXPENSE_BY_ID}") String URL_OF_GET_EXPENSE_BY_ID,
+                         @Value("${URL_OF_UPDATE_EXPENSE}") String URL_OF_UPDATE_EXPENSE,
+                         @Value("${URL_OF_DELETE_EXPENSE}") String URL_OF_DELETE_EXPENSE) {
         this.restTemplate = restTemplate;
+        this.URL_OF_FIND_ALL_EXPENSES = URL_OF_FIND_ALL_EXPENSES;
+        this.URL_OF_CREATE_EXPENSE = URL_OF_CREATE_EXPENSE;
+        this.URL_OF_GET_EXPENSE_BY_ID = URL_OF_GET_EXPENSE_BY_ID;
+        this.URL_OF_UPDATE_EXPENSE = URL_OF_UPDATE_EXPENSE;
+        this.URL_OF_DELETE_EXPENSE = URL_OF_DELETE_EXPENSE;
     }
 
     @Override
