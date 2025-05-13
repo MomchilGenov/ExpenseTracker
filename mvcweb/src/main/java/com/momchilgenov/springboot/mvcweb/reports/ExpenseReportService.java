b533a33e995a6912ddc479f136d85dcbfd367fdc
Momchil <momchilgenov2001@gmail.com>
2025-05-13 03:19:24 +0300

    ExpenseClient Di

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/reports/ExpenseReportService.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/reports/ExpenseReportService.java
@@ -1,6 +1,8 @@
 package com.momchilgenov.springboot.mvcweb.reports;
 
 import com.momchilgenov.springboot.mvcweb.expense.Expense;
+import com.momchilgenov.springboot.mvcweb.expense.ExpenseClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -10,11 +12,18 @@ import java.util.Map;
 @Service
 public class ExpenseReportService {
 
-    private List<Expense> getFilteredExpenses(LocalDate startDate, LocalDate endDate,String username) {
+    private final ExpenseClient expenseClient;
+
+    @Autowired
+    public ExpenseReportService(ExpenseClient expenseClient) {
+        this.expenseClient = expenseClient;
+    }
+
+    private List<Expense> getFilteredExpenses(LocalDate startDate, LocalDate endDate, String username) {
         return null;
     }
 
-    public Map<String, Double> getGroupedExpenses(String groupBy, List<Expense> filteredExpenses,String username) {
+    public Map<String, Double> getGroupedExpenses(String groupBy, List<Expense> filteredExpenses, String username) {
         return null;
     }
 
