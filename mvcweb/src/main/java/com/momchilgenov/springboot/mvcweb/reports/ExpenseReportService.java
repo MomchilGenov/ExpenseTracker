485019554b7d21631913b7fecc2eb94d92028d29
Momchil <momchilgenov2001@gmail.com>
2025-05-13 05:03:22 +0300

    getGroupedExpenses impl

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/reports/ExpenseReportService.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/reports/ExpenseReportService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -29,8 +31,56 @@ public class ExpenseReportService {
                 .toList();
     }
 
-    public Map<String, Double> getGroupedExpenses(String groupBy, List<Expense> filteredExpenses, String username) {
-        return null;
+    public Map<String, Double> getGroupedExpenses(LocalDate startDate, LocalDate endDate,
+                                                  String groupBy, String username) {
+        List<Expense> filteredExpenses = getFilteredExpenses(startDate, endDate, username);
+        Map<String, Double> groupedExpenses = null;
+        switch (groupBy) {
+            case "category":
+                groupedExpenses = filteredExpenses.stream()
+                        .collect(Collectors.groupingBy(
+                                expense -> expense.getCategory().getName(),
+                                Collectors.summingDouble(Expense::getAmount)
+                        ));
+                break;
+            case "day":
+                //day-month-year
+                groupedExpenses = filteredExpenses.stream()
+                        .sorted(Comparator.comparing(Expense::getDate))
+                        .collect(Collectors.groupingBy(
+                                expense -> expense.getDate().toString(),
+                                LinkedHashMap::new, // preserve sorting order in result
+                                Collectors.summingDouble(Expense::getAmount)
+                        ));
+
+                break;
+            case "month":
+                //month-year
+                groupedExpenses = filteredExpenses.stream()
+                        .sorted(Comparator.comparing(Expense::getDate))
+                        .collect(Collectors.groupingBy(
+                                expense -> expense.getDate().getMonth().toString() + " "
+                                        + String.valueOf(expense.getDate().getYear()),
+                                LinkedHashMap::new, // preserve sorting order in result
+                                Collectors.summingDouble(Expense::getAmount)
+                        ));
+
+                break;
+            case "year":
+                //year
+                groupedExpenses = filteredExpenses.stream()
+                        .sorted(Comparator.comparing(Expense::getDate))
+
+                        .collect(Collectors.groupingBy(
+                                expense -> String.valueOf(expense.getDate().getYear()),
+                                LinkedHashMap::new, // preserve sorting order in result
+                                Collectors.summingDouble(Expense::getAmount)
+                        ));
+
+                break;
+        }
+
+        return groupedExpenses;
     }
 
 }
