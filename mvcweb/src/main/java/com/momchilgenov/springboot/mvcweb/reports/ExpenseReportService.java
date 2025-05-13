555abc08e93bfcd7b68ede815cd242ee8fc98513
Momchil <momchilgenov2001@gmail.com>
2025-05-13 03:14:37 +0300

    Method stubs for getting grouped expenses by filter criteria

--- mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/reports/ExpenseReportService.java
+++ mvcweb/src/main/java/com/momchilgenov/springboot/mvcweb/reports/ExpenseReportService.java
@@ -1,7 +1,21 @@
 package com.momchilgenov.springboot.mvcweb.reports;
 
+import com.momchilgenov.springboot.mvcweb.expense.Expense;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ExpenseReportService {
+
+    private List<Expense> getFilteredExpenses(LocalDate startDate, LocalDate endDate,String username) {
+        return null;
+    }
+
+    public Map<String, Double> getGroupedExpenses(String groupBy, List<Expense> filteredExpenses,String username) {
+        return null;
+    }
+
 }
