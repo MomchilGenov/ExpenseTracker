0e2fad05758b2c1893466653a1f2c94e3bcc800e
Momchil <momchilgenov2001@gmail.com>
2025-05-16 16:28:51 +0300

    main method class

---
+++ dbcore/src/main/java/com/momchilgenov/dbcore/DbcoreApplication.java
@@ -0,0 +1,13 @@
+package com.momchilgenov.dbcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbcoreApplication.class, args);
+    }
+
+}
