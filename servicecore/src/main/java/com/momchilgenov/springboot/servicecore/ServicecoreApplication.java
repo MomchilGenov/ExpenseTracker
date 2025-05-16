d0ee95debad4af4a41643b8407861e227e3ebb00
Momchil <momchilgenov2001@gmail.com>
2025-05-16 16:25:15 +0300

    main method class

--- servicecore/src/main/java/com/momchilgenov/springboot/servicecore/ServicecoreApplication.java
+++ servicecore/src/main/java/com/momchilgenov/springboot/servicecore/ServicecoreApplication.java
@@ -1,14 +1,13 @@
 package com.momchilgenov.springboot.servicecore;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServicecoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServicecoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServicecoreApplication.class, args);
+    }
 }
